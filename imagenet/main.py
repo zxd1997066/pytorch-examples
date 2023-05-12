d4cb21091d238b2f530ce54213e353f256865154
zengxian <xiangdong.zeng@intel.com>
2023-05-12 10:20:34 +0800

    improve

--- imagenet/main.py
+++ imagenet/main.py
@@ -389,12 +389,13 @@ def validate(val_loader, model, criterion, args):
         with torch.no_grad():
             total_time = 0.0
             total_sample = 0
+            images = torch.randn(args.batch_size, 3, 224, 224)
+            if args.channels_last:
+                images = images.contiguous(memory_format=torch.channels_last)
             end = time.time()
             #for i, (images, target) in enumerate(loader):
             for i in range(args.num_warmup + args.num_iter):
                 images = torch.randn(args.batch_size, 3, 224, 224)
-                if args.channels_last:
-                    images = images.contiguous(memory_format=torch.channels_last)
                 if args.num_iter > 0 and i >= args.num_iter: break
                 i = base_progress + i
 
